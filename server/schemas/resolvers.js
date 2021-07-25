b7feb4328039d1dfed38cb99e186ff22becda954
Emily Gleeson <engleeson@gmail.com>
2021-07-25 19:39:41 -0400

    working on resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,50 @@
-const { User } = require('./typeDefs')
-const AuthenticationError = require('apollo-server-express')
+const { User, Book } = require('./typeDefs')
+const {AuthenticationError}  = require('apollo-server-express')
 const signToken = require('../utils/auth')
 
+const resolvers = {
+        Query: {
+            users: async () => {
+                return User.find();
+            },
+
+            user: async (parent, { username }) => {
+                return User.findOne({ name: username });
+            },
+
+            me: async (parent, args, context) => {
+                if (context.user) {
+                    return User.findOne({_id: context.user._id});
+                }
+             throw new AuthenticationError('You need to be logged in to see this!')
+            },
+
+        },
+
+    Mutation: {
+        login: async (parent, args) => {
+            const user = await User.findOne({ $or: [{ username: args.username}, {email: args.email}] })
+            if(!user) {
+                throw new AuthenticationError('No user found with these credentials!')
+            }
+
+            const correctPw = await user.isCorrectPassword(args.password);
+
+            if(!correctPw) {
+                throw new AuthenticationError('Incorrect password!')
+            }
+
+            const token = signToken(user);
+
+            return { token, user }
+        },
+
+        
+
+
+    }
+
+
+
+
+}
