2f0256bc7d41197624725dc3de8daa3f3e151746
Emily Gleeson <engleeson@gmail.com>
2021-07-25 20:02:00 -0400

    finished resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -38,13 +38,12 @@ const resolvers = {
 
             return { token, user }
         },
-
-        
-
-
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user }
+        }
     }
-
-
-
-
 }
+
+module.exports = resolvers;
